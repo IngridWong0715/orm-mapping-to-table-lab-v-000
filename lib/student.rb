f163c95c53bbedf8cfda7a63b7ff186413a2d353
Ingrid Wong <yw340@georgetown.edu>
2018-02-10 03:00:01 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,7 +18,7 @@ class Student
       SQL
 
     DB[:conn].execute(sql)
-    @id = 
+    @id =
   end
 
   def self.drop_table
@@ -33,7 +33,7 @@ class Student
       INSERT INTO students(name, grade) VALUES (?,?)
       SQL
     DB[:conn].execute(sql, self.name, self.grade)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
 
 
   end
