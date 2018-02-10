3e0abf7412da439b72f941922a8fb7ebc6813b8f
Ingrid Wong <yw340@georgetown.edu>
2018-02-10 02:42:05 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,12 +1,12 @@
 class Student
 
-attr_accessor :name, :grade
-attr_reader :id
+  attr_accessor :name, :grade
+  attr_reader :id
 
-def initialize(name, grade, id=nil)
-  @name = name
-  @grade = grade
-  @id = id
-end
+  def initialize(name, grade, id=nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
 
 end
